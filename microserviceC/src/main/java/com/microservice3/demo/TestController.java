a157ee2d0179028fdbd0bc883647f8dc5c4feeee
glinco <glinco.dev@gmail.com>
2026-09-20 23:35:52 -0300

    updates: service c deps, properties and test controller

---
+++ microserviceC/src/main/java/com/microservice3/demo/TestController.java
@@ -0,0 +1,14 @@
+package com.microservice3.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+ 
+    @GetMapping("/info")
+    public String getInfo() {
+        return "Hello from Service A!";
+    }
+}
+
